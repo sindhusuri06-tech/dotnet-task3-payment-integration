154846dd5ce422b813dc0780e71647581a9e6844
S. Sindhu <sindhusuri06@gmail.com>
2026-02-06 17:58:59 +0530

    Models → Payment.cs

---
+++ Models → Payment.cs
@@ -0,0 +1,9 @@
+namespace PaymentSystem.Models
+{
+    public class Payment
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Amount { get; set; }
+    }
+}
